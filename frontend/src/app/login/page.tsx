677c31da1a8856a5c2f8ddaef82728ad752b0253
rafhome <110781737+mhrafin@users.noreply.github.com>
2026-03-19 03:51:22 +0600

    feat: implement login page with validation and update create-form UI

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -1,3 +1,91 @@
+"use client";
+
+import { CalendarDays, SendHorizontal } from "lucide-react";
+import * as z from "zod";
+import { useForm, Controller } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+
+const loginFormSchema = z.object({
+  username: z.string(),
+  password: z.string(),
+});
 export default function LoginPage() {
-  return null;
+  const form = useForm<z.infer<typeof loginFormSchema>>({
+    resolver: zodResolver(loginFormSchema),
+    defaultValues: {
+      username: "",
+      password: "",
+    },
+  });
+  function onSubmit(data: z.infer<typeof loginFormSchema>) {
+    // Do something with the form values.
+    console.log(data);
+  }
+  return (
+    <div className="bg-muted text-primary m-4 p-4 rounded-xl">
+      <div className="flex flex-col">
+        <div className="bg-accent rounded-md self-center">
+          <CalendarDays className="m-1" />
+        </div>
+        <h1 className="self-center text-2xl">Welcome Back</h1>
+        <h2 className="text-center text-[15px] text-muted-foreground">
+          Please enter your details to sign in.
+        </h2>
+      </div>
+      <form id="login-form" onSubmit={form.handleSubmit(onSubmit)}>
+        <div className="flex flex-col gap-6 mt-6">
+          {/* Username */}
+          <Controller
+            name="username"
+            control={form.control}
+            render={({ field, fieldState }) => (
+              <div>
+                <Label htmlFor="username">Username</Label>
+                <Input
+                  {...field}
+                  id={field.name}
+                  className="mt-1"
+                  placeholder="Enter your username"
+                />
+              </div>
+            )}
+          />
+
+          {/* Password */}
+          <Controller
+            name="password"
+            control={form.control}
+            render={({ field, fieldState }) => (
+              <div>
+                <Label htmlFor="username">Password</Label>
+                <Input
+                  {...field}
+                  id={field.name}
+                  type="password"
+                  className="mt-1"
+                  placeholder="Enter your password"
+                />
+              </div>
+            )}
+          />
+          {/* Action Buttons */}
+          <div className="flex flex-row mt-1 gap-4 mb-2">
+            <div className="flex-1">
+              <Button
+                type="submit"
+                form="login-form"
+                variant="outline"
+                className="w-full bg-accent"
+              >
+                Sign In
+              </Button>
+            </div>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
 }
--- frontend/src/components/meeting/create-form.tsx
+++ frontend/src/components/meeting/create-form.tsx
@@ -8,20 +8,20 @@ import { SendHorizontal } from "lucide-react";
 
 const CreateMeetingForm = () => {
   return (
-    <div className="bg-blue-100 m-2 p-2 rounded-md">
-      <h1 className="text-3xl font-semibold">Create Meeting</h1>
-      <h2 className="font-stretch-ultra-condensed text-gray-500">
+    <div className="bg-muted text-primary m-4 p-4 rounded-xl">
+      <h1 className="text-3xl font-semibold mt-2">Create Meeting</h1>
+      <h2 className="font-stretch-ultra-condensed text-muted-foreground">
         Schedule a new session and define the agenda.
       </h2>
       <form>
-        <div className="flex flex-col gap-4 mt-4">
+        <div className="flex flex-col gap-6 mt-6">
           {/* Meeting Title */}
           <div>
             <Label htmlFor="meeting-title">Meeting Title</Label>
             <div>
               <Input
                 id="meeting-title"
-                className="bg-white mt-1"
+                className="mt-1"
                 placeholder="e.g., Q4 Product Roadmap Review"
               />
             </div>
@@ -30,21 +30,25 @@ const CreateMeetingForm = () => {
           <div>
             <Label htmlFor="date">Date</Label>
             <div className="relative">
-              <Input id="date" className="bg-white mt-1" type="date" />
+              <Input id="date" className="text-primary mt-1" type="date" />
             </div>
           </div>
           {/* Start Time */}
           <div>
             <Label htmlFor="start-time">Start Time</Label>
             <div>
-              <Input id="start-time" className="bg-white mt-1" type="time" />
+              <Input
+                id="start-time"
+                className="text-primary mt-1"
+                type="time"
+              />
             </div>
           </div>
           {/* End Time */}
           <div>
             <Label htmlFor="end-time">End Time</Label>
             <div>
-              <Input id="end-time" className="bg-white mt-1" type="time" />
+              <Input id="end-time" className="text-primary mt-1" type="time" />
             </div>
           </div>
           {/* Description */}
@@ -53,7 +57,7 @@ const CreateMeetingForm = () => {
             <div>
               <Textarea
                 id="description"
-                className="bg-white mt-1"
+                className="text-primary mt-1"
                 placeholder="Add agenda or meeting notes..."
               />
             </div>
@@ -64,14 +68,14 @@ const CreateMeetingForm = () => {
             <div className="relative mt-1">
               <Input
                 id="location"
-                className="bg-white pl-10"
+                className="text-primary pl-10"
                 placeholder="Link or room location"
               />
               <Video className="absolute left-3 top-1/2 -translate-y-1/2 size-4 pointer-events-none" />
             </div>
           </div>
           {/* Action Buttons */}
-          <div className="flex flex-row mt-1 gap-4">
+          <div className="flex flex-row mt-1 gap-4 mb-2">
             <div className="flex-1">
               <Button type="submit" variant="outline" className="w-full">
                 <SendHorizontal className="size-4 flex-none" />
@@ -79,7 +83,9 @@ const CreateMeetingForm = () => {
               </Button>
             </div>
 
-            <Button type="button" variant="outline">Cancel</Button>
+            <Button type="reset" variant="outline">
+              Cancel
+            </Button>
           </div>
         </div>
       </form>
