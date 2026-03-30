338ea8237d10e9dc7a6d9872374b01d3f21ef735
rafhome <110781737+mhrafin@users.noreply.github.com>
2026-03-30 08:16:53 +0600

    feat(frontend): implement registration page layout and form validation

--- frontend/src/app/register/page.tsx
+++ frontend/src/app/register/page.tsx
@@ -1,33 +1,49 @@
 "use client";
 
-import { useForm } from "react-hook-form";
+import { Controller, useForm } from "react-hook-form";
 import * as z from "zod";
-import { CalendarDays } from "lucide-react";
+import { CalendarDays, Divide } from "lucide-react";
 import {
   Avatar,
   AvatarFallback,
   AvatarGroup,
   AvatarImage,
-} from "@/components/ui/avatar"
+} from "@/components/ui/avatar";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
 
-const registerFormSchema = z.object({});
+const registerFormSchema = z.object({
+  email: z.string(),
+  username: z.string().min(1, "Username is required"),
+  password: z.string(),
+  confirm_pass: z.string(),
+});
 
 export default function RegisterPage() {
-  const form = useForm<z.infer<typeof registerFormSchema>>({});
+  const form = useForm<z.infer<typeof registerFormSchema>>({
+    resolver: zodResolver(registerFormSchema),
+  });
+
+  function onSubmit(data: z.infer<typeof registerFormSchema>) {
+    console.log(data);
+  }
+
   return (
     <div className="bg-surface">
-      <div className="m-4 rounded-xl flex flex-row bg-surface-container">
-        <div className="relative flex items-center">
-          <div className="bg-[url(/register/register.jpg)] bg-center bg-cover h-dvh w-dvh  rounded-tl-2xl rounded-bl-2xl opacity-80 absolute" />
-          <div className="absolute flex flex-col gap-6 p-8 z-10 text-white">
+      <div className="m-4 rounded-xl flex flex-row bg-surface-container h-dvh">
+        <div className="relative flex items-center w-1/2">
+          <div className="bg-[url(/register/register.jpg)] bg-center bg-cover h-full w-full rounded-tl-2xl rounded-bl-2xl opacity-80 absolute" />
+          <div className="bg-gradient-primary opacity-90 h-full w-full rounded-tl-xl rounded-bl-xl"></div>
+          <div className="absolute flex flex-col gap-6 p-10 z-10 text-primary-foreground">
             <div className="flex flex-row items-center mb-8">
-              <CalendarDays className="size-10 mr-2" />
-              <h3 className="text-5xl text-dispay">Schedula</h3>
+              <CalendarDays className="size-8 mr-2" />
+              <h3 className="text-4xl text-dispay">Schedula</h3>
             </div>
             <h3 className="text-4xl font-bold">
               Master your schedule, effortlessly
             </h3>
-            <p className="text-2xl">
+            <p>
               Join thousands of professionals who save hours every week by
               eliminating scheduling conflicts and endless email chains.
             </p>
@@ -38,24 +54,42 @@ export default function RegisterPage() {
                   <AvatarFallback>CN</AvatarFallback>
                 </Avatar>
                 <Avatar>
-                  <AvatarImage src="/register/avatar2.jpg" alt="@maxleiter" className="object-cover object-center" />
+                  <AvatarImage
+                    src="/register/avatar2.jpg"
+                    alt="@maxleiter"
+                    className="object-cover object-center"
+                  />
                   <AvatarFallback>LR</AvatarFallback>
                 </Avatar>
                 <Avatar>
-                  <AvatarImage
-                    src="/register/avatar3.jpg"
-                    alt="@evilrabbit"
-                  />
+                  <AvatarImage src="/register/avatar3.jpg" alt="@evilrabbit" />
                   <AvatarFallback>ER</AvatarFallback>
                 </Avatar>
               </AvatarGroup>
               <p className="text-sm">Over 50,000+ users</p>
             </div>
-
           </div>
-          <div className="bg-gradient-primary opacity-90 h-dvh w-dvh rounded-tl-xl rounded-bl-xl "></div>
         </div>
-        <div>Register Form</div>
+        <div className="flex-1 p-8">
+          <form id="register-form" onSubmit={form.handleSubmit(onSubmit)}>
+            <div className="flex flex-col gap-6">
+              <Controller
+                name="username"
+                control={form.control}
+                render={({ field, fieldState }) => (
+                  <div>
+                    <Label htmlFor="username">Username</Label>
+                    <Input
+                      {...field}
+                      id={field.name}
+                      placeholder="Enter a username"
+                    />
+                  </div>
+                )}
+              />
+            </div>
+          </form>
+        </div>
       </div>
     </div>
   );
